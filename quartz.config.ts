c6cdb365780b16a4fa924a0c9a581d8a460ca4af
Mico <miv1n@outlook.com>
2024-11-22 14:43:59 +0800

    允许 嵌入html,变更标题

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 CHATSEARCH-WIKI",
+    pageTitle: "🐲 CHATSEARCH",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
@@ -64,7 +64,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
